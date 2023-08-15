23dae2adb739c8805bbbb90b552e51a171decdbf
haeyng <141481323+haeyng@users.noreply.github.com>
2023-08-15 13:59:37 +0900

    [unrated] Title: 문자열 돌리기, Time: 157.73 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char [] as = a.toCharArray();   
+        
+        for (int i = 0; i < as.length; i++) {
+			System.out.println(as[i]);
+		} //for
+        
+    }
+}
