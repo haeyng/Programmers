77f4b44c6b102bd4059ac9139e62119acfd7af9f
haeyng <haeyng.song@gmail.com>
2023-08-15 14:06:34 +0900

    수정

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -4,6 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        /*
         switch (n%2) {
 		case 0:
 			System.out.printf("%d is even", n);
@@ -12,5 +13,8 @@ public class Solution {
 			System.out.printf("%d is odd", n);
 			break;
 		}//switch
+        */
+        System.out.print(n + " is " + (n%2==0?"even":"odd"));
+        //삼항연산자
     }
 }
