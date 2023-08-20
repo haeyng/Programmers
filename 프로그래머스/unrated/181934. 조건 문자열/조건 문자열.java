b2c29e8174418af180a2d2cccfca29098c600263
haeyng <141481323+haeyng@users.noreply.github.com>
2023-08-20 19:33:01 +0900

    Update 조건 문자열.java

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        int answer = 0;       
-		if (ineq.equals(">")) {        	          
+        int answer = 0;
+	    //문자열 비교는 == 대신 equals 사용
+		if (ineq.equals(">")) {        	        
             if(eq.equals("=")){
                 return n>=m ? 1 : 0;
             }else if(eq.equals("!")){
