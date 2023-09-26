7a7c3d2f1a7da3fd504024d5acef1e37ad26221b
haeyng <141481323+haeyng@users.noreply.github.com>
2023-09-27 08:36:01 +0900

    [unrated] Title: 카운트 업, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        
+        int[] answer = new int [end_num-start_num+1];
+        int n = 0;
+        
+        for(int i=start_num; i<=end_num; i++){
+            answer[n++] = i;     
+        }
+        return answer;
+    }
+}
